34b2dd1160f1f22c850b2e4794fef9e40ec84b87
milicaGovorcin <milica.govorcin@htecgroup.com>
2022-11-16 13:38:01 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import "./App.css";
-
-
-
+import './App.css';;;
 
 function App() {
     return (
-        <div className='App'><h1>Kraemer</h1>
+        <div className='App'>
+            <h1>Kraemer</h1>
         </div>
     );
 }
